7b58a3926814c4736e5cbd0eefd6dbd10e1e5cfe
juan <juan@juan-Aspire-5920.(none)>
2014-05-08 17:15:53 +0200

    Empezando con el ejercicio

---
+++ EjercicioBasicoPHPyHTML/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<title>EjercicioBasicoPHPyHTML</title>
+		<meta charset="UTF-8">
+		<meta name="autor" content="Juan Antonio Fenández Sánchez">
+		<meta name="descripcion" content="Ejercicio: empezando con la mezcla entre html y php">
+	</head>
+
+	<body>
+
+	</body>
+
+
+</html>
